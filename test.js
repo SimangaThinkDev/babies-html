9487e2f98d01256de55e6397d04933ca16a81db7
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-05-17 23:00:10 +0200

    Adding existing work, More to be added

---
+++ test.js
@@ -0,0 +1,7 @@
+function doSomething() {
+    if (document.getElementById("Greeting") == Hello) {
+        document.getElementById("Greeting").innerHTML = "Goodbye"
+    } else {
+        document.getElementById("Greeting").innerHTML = "Hello"
+    }
+}
